784691ed8bb3ce8a98dc10d94b070d74909d4b2c
Hugh <hughandreivirganio@gmail.com>
2026-07-18 22:13:29 +1200

    added a click outside to close feature for the modals

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function openModal(name) {
     document.getElementById('overlay-' + name).classList.add('open')
-    
+
 }
 
 function closeModal(name) {
     document.getElementById('overlay-' + name).classList.remove('open')
 }
+
+// click outside to close feature for a modal 
+document.getElementById('overlay-about').addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        closeModal('about')
+    }
+})
+
+
+// click outside to close feature for a modal 
+document.getElementById('overlay-projects').addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        closeModal('projects')
+    }
+})
+
+
+// click outside to close feature for a modal 
+document.getElementById('overlay-contact').addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        closeModal('contact')
+    }
+})
