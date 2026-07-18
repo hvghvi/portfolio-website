18a50b1da6b425261eeb561a6fde43ede8596168
Hugh <hughandreivirganio@gmail.com>
2026-07-18 22:17:59 +1200

    added comments in script.js. might remove feature soon ( will leave for later)

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function closeModal(name) {
     document.getElementById('overlay-' + name).classList.remove('open')
 }
 
+
+// these listeners should be updated !! 
+
 // click outside to close feature for a modal 
 document.getElementById('overlay-about').addEventListener('click', (e) => {
     if (e.target === e.currentTarget) {
@@ -29,3 +32,12 @@ document.getElementById('overlay-contact').addEventListener('click', (e) => {
         closeModal('contact')
     }
 })
+
+//document.addEventListener('keydown', (e) => {
+    //if (e.key === 'Escape') {
+   //     closeModal('about')
+   // }
+// these current listeners would not work when theres multiple open
+//feature might be removed in the future.
+//not needed at all!
+
